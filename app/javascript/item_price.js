6e64d9ccb1846c558d7b34d8bb3fa807338d20c8
MyShip <nichijouhappe6848@gmail.com>
2024-10-14 14:12:37 +0900

    item_listing_fixed_commit

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,19 +1,24 @@
-const priceInput  = document.getElementById("item-price");
-const addTaxDom   = document.getElementById("add-tax-price");
-const salesProfit = document.getElementById("profit");
+const price = () => {
+  const priceInput  = document.getElementById("item-price");
+  const addTaxDom   = document.getElementById("add-tax-price");
+  const salesProfit = document.getElementById("profit");
 
-priceInput.addEventListener('input', () => {
-  const inputValue = parseFloat(priceInput.value); // 入力値を数値に変換
-  if (isNaN(inputValue) || inputValue < 300 || inputValue > 9999999) {
-    addTaxDom.innerHTML = ''; // 無効な値の場合はクリア
-    salesProfit.innerHTML = '';
-    return;
-  }
-  const tax = Math.floor(inputValue * 0.1); // 販売手数料（10%）を計算
-  addTaxDom.innerHTML = tax.toLocaleString(); // 小数点切り捨て、表示
+  if (!priceInput) return; // 要素が存在しない場合は終了
 
-  const profit = inputValue - tax; // 販売利益を計算
-  salesProfit.innerHTML = profit.toLocaleString(); // 販売利益を表示
-});
+  priceInput.addEventListener('input', () => {
+    const inputValue = parseFloat(priceInput.value); // 入力値を数値に変換
+    if (isNaN(inputValue) || inputValue < 300 || inputValue > 9999999) {
+      addTaxDom.innerHTML = ''; // 無効な値の場合はクリア
+      salesProfit.innerHTML = '';
+      return;
+    }
 
+    const tax = Math.floor(inputValue * 0.1); // 販売手数料（10%）を計算
+    addTaxDom.innerHTML = tax.toLocaleString(); // 小数点切り捨て、表示
 
+    const profit = inputValue - tax; // 販売利益を計算
+    salesProfit.innerHTML = profit.toLocaleString(); // 販売利益を表示
+  });
+}
+document.addEventListener('turbo:load', price);
+document.addEventListener('turbo:render', price);
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,40 +11,60 @@ RSpec.describe Item, type: :model do
   end
   context '商品出品情報ができない時' do
     it '商品名が空では出品できない' do
+      @item.user = nil
+      expect(@item).not_to be_valid
+      expect(@item.errors[:user]).to include("must exist")
+    end
+
+    it '商品名が空では出品できない' do
       @item.name = ''
       expect(@item).not_to be_valid
       expect(@item.errors[:name]).to include("can't be blank")
     end
-
     it '商品の説明が空だと出品できない' do
       @item.explanation = ''
       expect(@item).not_to be_valid
       expect(@item.errors[:explanation]).to include("can't be blank")
     end
-    it '商品のカテゴリーが「---」の状態だと出品できない' do
-      @item.category_id = '---'
+    it '商品の画像が空だと出品できない' do
+      @item.image = nil
       expect(@item).not_to be_valid
-      expect(@item.errors[:category_id]).to include("is not a number")
+      expect(@item.errors[:image]).to include("can't be blank")
+    end
+    it '商品のカテゴリーが「---」の状態だと出品できない' do
+      @item.category_id = 1
+      expect(@item).not_to be_valid
+      expect(@item.errors[:category_id]).to include("must be other than 1")
     end
     it '商品の状態が「---」の状態だと出品できない' do
-      @item.situation_id = '---'
+      @item.situation_id = 1
       expect(@item).not_to be_valid
-      expect(@item.errors[:situation_id]).to include("is not a number")
+      expect(@item.errors[:situation_id]).to include("must be other than 1")
     end
     it '配送料の負担が「---」の状態だと出品できない' do
-      @item.load_id = '---'
+      @item.load_id = 1
       expect(@item).not_to be_valid
-      expect(@item.errors[:load_id]).to include("is not a number")
+      expect(@item.errors[:load_id]).to include("must be other than 1")
     end
     it '配送先の地域が「---」の状態だと出品できない' do
-      @item.prefecture_id  = '---'
+      @item.prefecture_id  = 1
       expect(@item).not_to be_valid
-      expect(@item.errors[:prefecture_id]).to include("is not a number")
+      expect(@item.errors[:prefecture_id]).to include("must be other than 1")
     end
     it '配送までの日数が「---」の状態だと出品できない' do
-      @item.day_id = '---'
+      @item.day_id = 1
       expect(@item).not_to be_valid
-      expect(@item.errors[:day_id]).to include("is not a number")
+      expect(@item.errors[:day_id]).to include("must be other than 1")
+    end
+    it '商品の値段が空欄だと出品できない' do
+      @item.price = ''
+      expect(@item).not_to be_valid
+      expect(@item.errors[:price]).to include("can't be blank", "is not a number")
+    end
+    it '商品の値段に半角英数字以外が入力されていると出品できない' do
+      @item.price = 'ああ'
+      expect(@item).not_to be_valid
+      expect(@item.errors[:price]).to include("is not a number")
     end
     it '商品の値段が300より小さいと出品できない' do
       @item.price = '100'
