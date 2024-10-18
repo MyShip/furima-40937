e48989315bd8a5350bc98550d9fc7378a16a1ae4
MyShip <nichijouhappe6848@gmail.com>
2024-10-18 13:29:35 +0900

    item_details_display_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :items, only:[:index, :new, :create]
+  resources :items, only:[:index, :new, :create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
